e1766aeac0bcee65272d927535e06f1e5c0e9ffb
River <riverehill@gmail.com>
2021-02-13 17:27:46 -0800

    things

---
+++ HW6/show.h
@@ -0,0 +1,14 @@
+#ifndef SHOW_H
+#define SHOW_H
+#include <iostream>
+
+class Show
+{
+public:
+    String(string name, string genre) 
+        : string(string), genre(genre) {}
+    string name;
+    string genre;
+};
+
+#endif /* SHOW_H */
